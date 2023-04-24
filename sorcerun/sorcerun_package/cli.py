c08c2010228de22acfc63649a9815ec44c44be21
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2023-04-23 17:30:58 -0700

    Add barebones package

---
+++ sorcerun/sorcerun_package/cli.py
@@ -0,0 +1,11 @@
+import click
+
+
+@click.command()
+@click.option("--name", default="World", help="Name to greet.")
+def hello(name):
+    click.echo(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    hello()
