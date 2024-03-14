6a68c746f284e1c61661096e32fc0c4122ab886e
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2024-03-14 11:32:02 -0700

    Bump to 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sorcerun",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     install_requires=[
         "click",
