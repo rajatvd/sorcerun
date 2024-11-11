5edc3beda21193f3987105c255938c647dfc9985
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2024-11-11 11:51:54 -0800

    bump version -- breaking changes netcdf -> csv

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sorcerun",
-    version="0.4.7",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[
         "click",
