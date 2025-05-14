1dcf5c25e1c14d89b89c215e01dca750908c1ff9
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2025-05-13 23:17:04 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chmod("scripts/flamegraph.pl", 0o755)
 
 setup(
     name="sorcerun",
-    version="0.5.13",
+    version="0.5.14",
     packages=find_packages(),
     install_requires=[
         "click",
